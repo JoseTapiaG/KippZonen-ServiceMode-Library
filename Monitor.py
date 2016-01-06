62091c7e0670682c37a5d51d60542abbb0bd8664
JoseTapiaG <jose.wt@gmail.com>
2016-01-06 14:32:47 -0300

    Se agrega Monitor, clase encargada de pedir y ordenar la informacion almacenada en tarjeta sd

---
+++ Monitor.py
@@ -0,0 +1,46 @@
+from Data import Data
+from LogBoxSD import LogBoxSD
+
+
+class Monitor:
+    def __init__(self):
+        self.logBoxSD = LogBoxSD()
+
+    def getData(self, date):
+
+        self.logBoxSD.serviceModeState()
+        self.logBoxSD.sdMode()
+        fileNumber = self.__getFileNumber(date)
+        return self.__parseData(self.logBoxSD.getFileData(fileNumber))
+
+    def __getFileNumber(self, date):
+        for fileName in self.logBoxSD.listFiles():
+            if date == self.__getDateOfFileName(fileName):
+                return self.__getFileNumberOfFileName(fileName)
+        return -1
+
+    def __getDateOfFileName(self, str):
+        if "SD card size:" in str:
+            return -1
+        split = str.split(" ")
+        return split[3]
+
+    def __getFileNumberOfFileName(self, str):
+        return int(str.split(" ")[0][4:-4])
+
+    def __parseData(self, registers):
+        parsedRegisters = []
+        for register in registers:
+            parsedRegisters.append(self.__parseRegister(register))
+        return parsedRegisters
+
+    def __parseRegister(self, split):
+        data = Data(
+                date=split[0],
+                time=split[1],
+                data1=split[3],
+                data2=split[6],
+                data3=split[9]
+        )
+
+        return data
