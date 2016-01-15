c5cfc259d754f6a14cf9a380ab2d2fe13b8cdcd3
sebastian <sebablasko@gmail.com>
2016-01-14 22:02:10 -0300

    Added TCPConnectionError Exception

--- Exceptions.py
+++ Exceptions.py
@@ -1,1 +1,19 @@
-class MethodNotFound : Exception
+# -*- coding: utf-8 -*-
+
+"""
+    Exceptions raised in TCPConnection class
+"""
+
+
+class TCPConnectionError(Exception):
+    def __init__(self):
+        print "Connection refused by host"
+
+"""
+    Exceptions raised in LogBoxSD class
+"""
+
+
+class MethodNotFound(Exception):
+    def __init__(self):
+        print "Method not found"
