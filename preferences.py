63a6fedfc1525d5207d6ebc04326509d45f981aa
JoseTapiaG <jose.wt@gmail.com>
2016-01-15 11:58:22 -0300

    Borrar preferencias de libreria, parametros se reciben en metodos

--- preferences.py
+++
@@ -1,9 +0,0 @@
-import serial #module pyserial - sudo pip install pyserial
-
-connectionClass="tty"
-moduleName="tty"
-port='/dev/ttyUSB1',
-baudrate=115200,
-parity=serial.PARITY_ODD,
-stopbits=serial.STOPBITS_TWO,
-bytesize=serial.EIGHTBITS
