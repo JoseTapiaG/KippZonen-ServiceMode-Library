3e514b89be69c01e32c8ae7e2525cdaa486c18d3
JoseTapiaG <jose.wt@gmail.com>
2016-01-15 11:25:56 -0300

    Print para python3

--- Exceptions.py
+++ Exceptions.py
@@ -8,14 +8,14 @@
 # Exceptions raised in LogBoxSD class
 class MethodNotFound(Exception):
     def __init__(self):
-        print "Error: Method not found"
+        print("Error: Method not found")
 
 # Exceptions raised in TCPConnection class
 class TCPConnectionError(Exception):
     def __init__(self):
-        print "Error: Connection refused by host"
+        print("Error: Connection refused by host")
 
 # Exceptions raised in TTYConnection class
 class TTYConnectionError(Exception):
     def __init__(self, msg):
-        print "Error: ", msg
+        print("Error: ", msg)
