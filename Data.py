2785152880c77144c5c74b875802c7848faafbee
JoseTapiaG <jose.wt@gmail.com>
2016-01-15 11:26:46 -0300

    Borrar datos iniciales en campos

--- Data.py
+++ Data.py
@@ -1,15 +1,7 @@
 class Data:
-
-    date = 0
-    time = 0
-    data1 = 0
-    data2 = 0
-    data3 = 0
-
     def __init__(self, date, time, data1, data2, data3):
         self.date = date
         self.time = time
         self.data1 = data1
         self.data2 = data2
         self.data3 = data3
-
